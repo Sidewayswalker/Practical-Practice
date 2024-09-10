6d9a64fccc947e529ec634cc8c036dde887c7495
Steven Karl <Karl.Steven09@gmail.com>
2024-09-10 16:37:18 -0500

    Question 3

--- src/components/Q3/Q3.js
+++ src/components/Q3/Q3.js
@@ -1,16 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import '../Q3/Q3.css'
+import React, { useEffect, useState } from 'react';
+import '../Q3/Q3.css';
 
-function Q3 (){
+// FizzBuzz Component
 
+function Q3() {
+    // State to store the FizzBuzz results
+    const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        // Generate FizzBuzz results
+        const fizzBuzzResults = [];
+        for (let i = 1; i <= 15; i++) {
+            if (i % 15 === 0) fizzBuzzResults.push("FizzBuzz");
+            else if (i % 3 === 0) fizzBuzzResults.push("Fizz");
+            else if (i % 5 === 0) fizzBuzzResults.push("Buzz");
+            else fizzBuzzResults.push(i);
+        }
+        // Update state with the results
+        setResults(fizzBuzzResults);
+    }, []);
 
-
-
-    return(
+    return (
         <div>
-            <p></p>
+            <p>3. Write a program that prints numbers 1 - 15, 
+                but for multiples of 3, print "Fizz" and 
+                for multiples of 5, print "Buzz."
+            </p>
+            <ul className='Q3-list'>
+                {results.map((result, index) => (
+                    <li key={index}>{result}</li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
-export default Q3
+
+export default Q3;
